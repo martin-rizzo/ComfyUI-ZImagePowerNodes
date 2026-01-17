ed8900cd18e6700f62a30145d63d70ebfebc2141
Martin Rizzo <martinrizzo@gmail.com>
2026-01-17 01:15:51 -0300

    feat: Enhance "ZSampler Turbo" node with improved progress bar and input widgets
    This commit refines the "ZSampler Turbo" node by incorporating the `ProgressPreview`
    class to accurately display progress through its nine steps. Additionally, it adds
    user interface widgets for entering 'steps' and 'denoise' parameters, though these
    inputs are currently placeholders for future enhancements that will allow more
    customizable control over the denoising process of ZSampler.

--- nodes/zsampler_turbo.py
+++ nodes/zsampler_turbo.py
@@ -10,18 +10,18 @@ License : MIT
              Experimental ComfyUI nodes for the Z-Image model.
 _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
 
-    The V3 schema documentation can be found here:
-    - https://docs.comfy.org/custom-nodes/v3_migration
+  ComfyUI V3 schema documentation can be found here:
+  - https://docs.comfy.org/custom-nodes/v3_migration
 
+_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
 """
 import torch
 import comfy.utils
 import comfy.sample
 import comfy.samplers
-import comfy.nested_tensor
-import latent_preview
-from comfy_api.latest import io
-from .core.system     import logger
+from comfy_api.latest   import io
+from .core.system       import logger
+from .core.progress_bar import ProgressPreview
 
 
 class ZSamplerTurbo(io.ComfyNode):
@@ -40,24 +40,53 @@ class ZSamplerTurbo(io.ComfyNode):
             is_deprecated = cls.xDEPRECATED,
             inputs=[
                 io.Model.Input       ("model"       , tooltip="The model used for generating the latent images."),
-                io.Conditioning.Input("positive"    , tooltip="The conditional text prompts to embed in the latent image."),
-                io.Latent.Input      ("latent_input", tooltip="The latent image to denoise."),
-                io.Int.Input         ("seed"        , tooltip="The seed to use for the random number generator."),
+                io.Conditioning.Input("positive"    , tooltip="The conditioning used to guide the generation process toward the desired content."),
+                io.Latent.Input      ("latent_input", tooltip="The initial latent image to be modified; typically an 'Empty Latent' for text-to-image or an encoded image for img2img."),
+                io.Int.Input         ("seed"        , tooltip="The seed used for the random noise generator, ensuring the same result is produced with the same value.",
+                                                      default=0, min=0, max=0xffffffffffffffff, control_after_generate=True),
+                io.Int.Input         ("steps"       , tooltip="The number of iterations to be performed during the sampling process.",
+                                                      default=9, min=7, max=9, step=1),
+                io.Float.Input       ("denoise"     , tooltip="The amount of denoising applied, lower values will maintain the structure of the initial image allowing for image to image sampling.",
+                                                      default=1.0, min=0.0, max=1.0, step=0.01),
             ],
             outputs=[
-                io.Latent.Output(display_name="latent_output", tooltip="The latent image after denoising."),
+                io.Latent.Output(display_name="latent_output", tooltip="The resulting denoised latent image, ready to be decoded by a VAE or passed to another sampler."),
             ]
         )
 
     #__ FUNCTION __________________________________________
     @classmethod
-    def execute(cls, model, positive, latent_input, seed) -> io.NodeOutput:
-        sampler = comfy.samplers.sampler_object("euler")
-        latent_input , _ = cls.execute_sampler_custom(model, True , seed, 1.0, positive, positive, sampler, [0.991, 0.98, 0.92], latent_input)
-        latent_input , _ = cls.execute_sampler_custom(model, False, seed, 1.0, positive, positive, sampler, [0.935, 0.90, 0.875, 0.750, 0.0000], latent_input)
-        latent_output, _ = cls.execute_sampler_custom(model, True , 696969, 1.0, positive, positive, sampler, [0.6582, 0.4556, 0.2000, 0.0000], latent_input)
+    def execute(cls,
+                model,
+                positive    : list,
+                latent_input: dict[str, any],
+                seed        : int,
+                steps       : int,
+                denoise     : float,
+                ) -> io.NodeOutput:
+
+        sampler  = comfy.samplers.sampler_object("euler")
+        progress = ProgressPreview.from_comfyui(model, 9)
+        latent_output = cls.execute_sampler_custom(model, True, seed, 1.0, positive, positive, sampler,
+                                                   sigmas           = [0.991, 0.98, 0.92],
+                                                   latent_image     = latent_input,
+                                                   progress_preview = ProgressPreview(2, parent=(progress,0,2)),
+                                                   )
+        latent_output = cls.execute_sampler_custom(model, False, seed, 1.0, positive, positive, sampler,
+                                                   sigmas           = [0.935, 0.90, 0.875, 0.750, 0.0000],
+                                                   latent_image     = latent_output,
+                                                   progress_preview = ProgressPreview(4, parent=(progress,2,6)),
+                                                   )
+        latent_output = cls.execute_sampler_custom(model, True, 696969, 1.0, positive, positive, sampler,
+                                                   sigmas           = [0.6582, 0.4556, 0.2000, 0.0000],
+                                                   latent_image     = latent_output,
+                                                   progress_preview = ProgressPreview(3, parent=(progress,6,9)),
+                                                   )
         return io.NodeOutput(latent_output)
 
+
+
+    #__ internal functions ________________________________
 
     @classmethod
     def execute_sampler_custom(cls,
@@ -69,50 +98,43 @@ class ZSamplerTurbo(io.ComfyNode):
                                negative,
                                sampler,
                                sigmas,
-                               latent_image
-                               ) -> io.NodeOutput:
+                               latent_image : dict[str, any],
+                               *,
+                               progress_preview: ProgressPreview | None = None,
+                               ) -> dict[str, any]:
 
             # if sigmas is a list then convert it to pytorch tensor
             if isinstance(sigmas, list):
-                samples = latent_image["samples"]
                 sigmas = torch.tensor(sigmas, device='cpu')
-                #.to(samples.device)
 
-            # generate a copy of the latent and at the same time fix empty channels
-            latent = latent_image
-            latent_image = latent["samples"]
-            latent = latent.copy()
-            latent_image = comfy.sample.fix_empty_latent_channels(model, latent_image)
-            latent["samples"] = latent_image
-
+            # extract all the info that comes packaged in the `latent_image` dictionary
+            latent      = latent_image.copy()
+            samples     = comfy.sample.fix_empty_latent_channels(model, latent["samples"])
+            noise_mask  = latent.get("noise_mask")
+            batch_index = latent.get("batch_index")
             if not add_noise:
-                samples = latent["samples"]
-                noise   = torch.zeros(samples.shape, dtype=samples.dtype, layout=samples.layout, device="cpu")
+                noise = torch.zeros(samples.shape, dtype=samples.dtype, layout=samples.layout, device="cpu")
             else:
-                samples = latent["samples"]
-                noise   = comfy.sample.prepare_noise(samples, noise_seed, latent.get("batch_index"))
+                noise = comfy.sample.prepare_noise(samples, noise_seed, batch_index)
 
-            noise_mask = None
-            if "noise_mask" in latent:
-                noise_mask = latent["noise_mask"]
-
-            x0_output = {}
-            callback = latent_preview.prepare_callback(model, sigmas.shape[-1] - 1, x0_output)
+            #x0_output = {}
+            #callback = latent_preview.prepare_callback(model, sigmas.shape[-1] - 1, x0_output)
 
             disable_pbar = not comfy.utils.PROGRESS_BAR_ENABLED
-            samples = comfy.sample.sample_custom(model, noise, cfg, sampler, sigmas, positive, negative, latent_image, noise_mask=noise_mask, callback=callback, disable_pbar=disable_pbar, seed=noise_seed)
+            samples = comfy.sample.sample_custom(model, noise, cfg, sampler, sigmas, positive, negative, samples, noise_mask=noise_mask, callback=progress_preview, disable_pbar=disable_pbar, seed=noise_seed)
 
-            out = latent.copy()
+            out = latent_image.copy()
             out["samples"] = samples
-            if "x0" in x0_output:
-                x0_out = model.model.process_latent_out(x0_output["x0"].cpu())
-                if samples.is_nested:
-                    latent_shapes = [x.shape for x in samples.unbind()]
-                    x0_out = comfy.nested_tensor.NestedTensor(comfy.utils.unpack_latents(x0_out, latent_shapes))
-                out_denoised = latent.copy()
-                out_denoised["samples"] = x0_out
-            else:
-                out_denoised = out
+            return out
 
-            return (out, out_denoised)
+            # if "x0" in x0_output:
+            #     x0_out = model.model.process_latent_out(x0_output["x0"].cpu())
+            #     if samples.is_nested:
+            #         latent_shapes = [x.shape for x in samples.unbind()]
+            #         x0_out = comfy.nested_tensor.NestedTensor(comfy.utils.unpack_latents(x0_out, latent_shapes))
+            #     out_denoised = latent.copy()
+            #     out_denoised["samples"] = x0_out
+            # else:
+            #     out_denoised = out
+            # return (out, out_denoised)
 
